8ce817cd0a28ddf0b7121251b5f2f88f8388a2f6
Tiago Murakami <trmurakami@gmail.com>
2019-11-26 13:33:07 -0300

    Initial

--- index.php
+++ index.php
@@ -22,6 +22,27 @@
         <meta property="og:image:height" content="600"> 
         <meta property="og:type" content="website">
         <!-- Facebook Tags - END -->
+
+        <style>
+            .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            }
+            @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+            }
+            .jumbotron {
+            background-image: url("<?php echo $background_1 ?>");
+            background-size: 100%;
+            background-repeat: no-repeat;            
+            }    
+        </style>
         
     </head>
 
@@ -34,8 +55,8 @@
     <!-- /NAV --> 
 
     <div class="jumbotron">
-        <div class="container">
-            <h1 class="display-5 mt-3"><?php echo $branch; ?></h1>
+        <div class="container bg-light p-5 rounded">
+            <h1 class="display-5"><?php echo $branch; ?></h1>
             <p><?php echo $branch_description; ?></p>
 
             <?php isset($error_connection_message) ? print_r($error_connection_message) : "" ?>
