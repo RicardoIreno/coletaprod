302cdbd2a3c6d3b308f58dc619c8336729ac9184
Tiago Murakami <trmurakami@gmail.com>
2019-12-04 10:11:50 -0300

    Initial

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="<?php echo "$url_base/"?>">Coletaprod ECA/USP</a>
+        <a class="navbar-brand" href="index.php"><?php echo $branch; ?></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
