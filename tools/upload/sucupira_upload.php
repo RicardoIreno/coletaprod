8ae7c1b4e1ea6a604e4ed996c440db9bcf417036
Tiago Murakami <trmurakami@gmail.com>
2020-05-15 11:33:11 -0300

    Initial

--- tools/upload/sucupira_upload.php
+++ tools/upload/sucupira_upload.php
@@ -15,6 +15,30 @@ if (isset($_FILES['file'])) {
         if ($value == "ID_PRODUCAO_INTELECTUAL") {
             $rowNum["ID_PRODUCAO_INTELECTUAL"] = $key;
         }
+        if ($value == "NM_SUBTIPO_PRODUCAO") {
+            $rowNum["NM_SUBTIPO_PRODUCAO"] = $key;
+        }
+        if ($value == "NM_PROGRAMA_IES") {
+            $rowNum["NM_PROGRAMA_IES"] = $key;
+        }
+        if ($value == "NM_AREA_CONCENTRACAO") {
+            $rowNum["NM_AREA_CONCENTRACAO"] = $key;
+        }
+        if ($value == "NM_LINHA_PESQUISA") {
+            $rowNum["NM_LINHA_PESQUISA"] = $key;
+        }
+        if ($value == "NM_PROJETO") {
+            $rowNum["NM_PROJETO"] = $key;
+        }
+        if ($value == "AN_BASE_PRODUCAO") {
+            $rowNum["AN_BASE_PRODUCAO"] = $key;
+        }
+        if ($value == "ID_ADD_PRODUCAO_INTELECTUAL") {
+            $rowNum["ID_ADD_PRODUCAO_INTELECTUAL"] = $key;
+        }
+        if ($value == "DS_DOI") {
+            $rowNum["DS_DOI"] = $key;
+        }                    
     }
 
     while (($row = fgetcsv($fh, 8192, "\t")) !== false) {
@@ -26,10 +50,10 @@ if (isset($_FILES['file'])) {
         $sha256 = hash('sha256', ''.$doc["doc"]["source_id"].'');
         print_r($sha256);
         print("<pre>".print_r($doc, true)."</pre>");
-        // if (!is_null($sha256)) {
-        //     $resultado_scopus = Elasticsearch::update($sha256, $doc);
-        // }
-        // print_r($resultado_scopus);
+        if (!is_null($sha256)) {
+           $resultado = Elasticsearch::update($sha256, $doc);
+        }
+        print_r($resultado);
         // print_r($doc["doc"]["source_id"]);
         // echo "<br/><br/><br/>";
         flush();
@@ -47,18 +71,36 @@ class Record
         $doc["doc"]["type"] = "Work";
         $doc["doc"]["source"] = "Sucupira";
         $doc["doc"]["match"]["tag"][] = "Sucupira";
-        $doc["doc"]["name"] = str_replace('"', '', $row[$rowNum["NM_PRODUCAO"]]);
-        // $doc["doc"]["datePublished"] = $row[$rowNum["year"]];
-        $doc["doc"]["source_id"] = $row[$rowNum["ID_PRODUCAO_INTELECTUAL"]];
+
+        if (isset($row[$rowNum["NM_PROGRAMA_IES"]])) {
+            $doc['doc']["instituicao"]['ppg_nome'] = $row[$rowNum["NM_PROGRAMA_IES"]];
+        }        
+        if (!empty($row[$rowNum["NM_AREA_CONCENTRACAO"]])) {
+            $doc['doc']["sucupira"]['area_concentracao'] = $row[$rowNum["NM_AREA_CONCENTRACAO"]];
+        }
+        if (isset($row[$rowNum["NM_LINHA_PESQUISA"]])) {
+            $doc['doc']["sucupira"]['linha_pesquisa'] = $row[$rowNum["NM_LINHA_PESQUISA"]];
+        }
+        if (isset($row[$rowNum["NM_PROJETO"]])) {
+            $doc['doc']["sucupira"]['projeto'] = $row[$rowNum["NM_PROJETO"]];
+        }
+        if (isset($row[$rowNum["NM_PRODUCAO"]])) {
+            $doc["doc"]["name"] = str_replace('"', '', $row[$rowNum["NM_PRODUCAO"]]);
+        }
+        if (isset($row[$rowNum["AN_BASE_PRODUCAO"]])) {
+            $doc["doc"]["datePublished"] = $row[$rowNum["AN_BASE_PRODUCAO"]];
+        }
+        if (isset($row[$rowNum["NM_SUBTIPO_PRODUCAO"]])) {
+            $doc["doc"]["tipo"] = $row[$rowNum["NM_SUBTIPO_PRODUCAO"]];
+        }
+        $doc["doc"]["source_id"] = $row[$rowNum["ID_ADD_PRODUCAO_INTELECTUAL"]];
         $doc["doc"]["tag"][] = $tag;
-        // if (!empty($row[$rowNum["DOI"]]) && $row[$rowNum["DOI"]] != "") {
-        //   $doc["doc"]["doi"] = $row[$rowNum["DOI"]];
-        // }
+
+        if (!empty($row[$rowNum["DS_DOI"]]) && $row[$rowNum["DS_DOI"]] != "") {
+           $doc["doc"]["doi"] = $row[$rowNum["DS_DOI"]];
+        }
         // $doc["doc"]["language"] = $row[$rowNum["language"]];
         // $doc["doc"]["description"] = $row[$rowNum["Abstract"]];
-
-        // $doc["doc"]["tipo"] = $row[$rowNum["type"]];
-
         // $doc["doc"]["isPartOf"]["name"] = strtoupper($row[$rowNum["sourceTitle"]]);
         // $doc["doc"]["isPartOf"]["volume"] = $row[$rowNum["Volume"]];
         // $doc["doc"]["isPartOf"]["fasciculo"] = $row[$rowNum["Issue"]];
@@ -69,8 +111,6 @@ class Record
         // $doc["doc"]["citedby"] = $row[$rowNum["CitedBy"]];
         // $doc["doc"]["scopus"]["citedby"] = $row[$rowNum["CitedBy"]];
         // $doc["doc"]["scopus"]["references"] = $row[$rowNum["References"]];
-
-
         // // Agência de fomento
         // $agencia_de_fomento_array = explode(";", $row[$rowNum["FundingDetails"]]);
         // $i_funder = 0;
@@ -86,7 +126,6 @@ class Record
         //     }
         //     $i_funder++;
         // }
-
         // // Palavras chave
         // $palavras_chave_authors = explode(";", $row[$rowNum["AuthorKeywords"]]);
         // $palavras_chave_scopus = explode(";", $row[$rowNum["IndexKeywords"]]);
