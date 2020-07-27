c40a1439ef35c55ff90b37f3dc8e1f6167582cca
Tiago Murakami <trmurakami@gmail.com>
2020-07-27 20:21:04 -0300

    Initial

--- tools/upload/upload_candidados.php
+++ tools/upload/upload_candidados.php
@@ -6,13 +6,13 @@ require '../../inc/functions.php';
 var_dump($argv);
 
 $row = 1;
-if (($handle = fopen("teste.csv", "r")) !== FALSE) {
+if (($handle = fopen($argv[1], "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
         echo "<p> $num campos na linha $row: <br /></p>\n";
         $row++;
         for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "<br />\n";
+            echo $data[$c] . "\n";
         }
     }
     fclose($handle);
