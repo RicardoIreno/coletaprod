9141e4ac7c5c8c27782cc1c415c1de1814b7f829
Tiago Murakami <trmurakami@gmail.com>
2020-07-28 10:54:54 -0300

    Initial

--- tools/upload/upload_csv.php
+++ tools/upload/upload_csv.php
@@ -5,15 +5,19 @@ require '../../inc/functions.php';
 
 $row = 1;
 if (($handle = fopen($argv[1], "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        echo "<p> $num campos na linha $row: <br /></p>\n";
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "\n";
+    while (($data = fgetcsv($handle, 1000, $argv[2])) !== FALSE) {
+        if (!defined('CONSTANT_ARRAY')) {
+            define("CONSTANT_ARRAY", $data);
+        } else {
+            $num = count($data);
+            for ($c=0; $c < $num; $c++) {
+                $doc["doc"][CONSTANT_ARRAY[$c]] = $data[$c];
+            }
         }
+        $doc["doc_as_upsert"] = true;
+        print_r($doc);
+        unset($doc);
     }
     fclose($handle);
 }
-
 ?>
