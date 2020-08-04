c7191445c63437ae3a0e788713c4de75f20d68f6
Tiago Murakami <trmurakami@gmail.com>
2020-08-04 20:50:40 -0300

    Initial

--- tools/upload/upload_csv.php
+++ tools/upload/upload_csv.php
@@ -16,29 +16,32 @@ try {
 }
 if (isset($argv[3]) && $testIndex == false) {
     Elasticsearch::createIndex($argv[3], $client);
-    //Elasticsearch::mappingsIndex($index, $client);
 }
 
+$line = fgets(fopen($argv[1], 'r'));
+$mappingsArray = explode($argv[2], $line);
+define("CONSTANT_ARRAY", $mappingsArray);
+
+foreach ($mappingsArray as $mappingString) {
+    $mappingsParamsArray[$mappingString]["type"] = "text";
+}
+
+$mappingsParams["index"] = $argv[3];
+$mappingsParams["body"]["properties"] = $mappingsParamsArray;
+$client->indices()->putMapping($mappingsParams);
 
 $row = 1;
 if (($handle = fopen($argv[1], "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, $argv[2])) !== FALSE) {
-        if (!defined('CONSTANT_ARRAY')) {
-            define("CONSTANT_ARRAY", $data);
-        } else {
-            $num = count($data);
-            for ($c=0; $c < $num; $c++) {
-                $docArray[CONSTANT_ARRAY[$c]] = $data[$c];
-                $doc["doc"] = array_filter($docArray);
-                $doc["doc_as_upsert"] = true;
-            }
+        $num = count($data);
+        for ($c=1; $c < $num; $c++) {
+            $docArray[CONSTANT_ARRAY[$c]] = $data[$c];
+            $doc["doc"] = array_filter($docArray);
+            $doc["doc_as_upsert"] = true;
         }
-        print_r($doc);
         unset($sha256);
+        $sha256 = hash('sha256', ''.$doc["doc"]["nome"].''.$doc["doc"]["ano_eleicao"].''.$doc["doc"]["sigla_uf"].''.$doc["doc"]["cpf"].''.$doc["doc"]["titulo_eleitoral"].'');
         if (!is_null($doc)) {
-            $sha256 = hash('sha256', ''.$doc["doc"]["numero_processo_candidatura"].'');
-        }
-        if (!is_null($sha256)) {
             $resultado = Elasticsearch::update($sha256, $doc, $argv[3]);
         }
         print_r($resultado);
