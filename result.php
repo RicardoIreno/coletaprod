5a753c72b3297d5df03865da813c3b0965c7765a
Tiago Murakami <trmurakami@gmail.com>
2020-11-20 10:37:36 -0300

    Retirado facetas duplicadas

--- result.php
+++ result.php
@@ -271,9 +271,6 @@ $get_data = $_GET;
                     $facets->facet(basename(__FILE__), "author.person.name", 100, "Nome completo do autor", null, "_term", $_GET);
                     $facets->facet(basename(__FILE__), "lattes_ids", 100, "Número do lattes", null, "_term", $_GET);
                     $facets->facet(basename(__FILE__), "vinculo.nome",100,"Nome do autor vinculado à instituição",null,"_term",$_GET);
-                    $facets->facet(basename(__FILE__), "vinculo.unidade",100,"Unidade",null,"_term",$_GET);
-                    $facets->facet(basename(__FILE__), "vinculo.departamento",100,"Departamento",null,"_term",$_GET);
-                    $facets->facet(basename(__FILE__), "vinculo.tipvin", 100, "Tipo de vínculo", null, "_term", $_GET);
                     
                     $facets->facet(basename(__FILE__), "country",200,"País de publicação",null,"_term",$_GET);
                     $facets->facet(basename(__FILE__), "datePublished",120,"Ano de publicação","desc","_term",$_GET);
