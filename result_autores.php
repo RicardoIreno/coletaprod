43c947309362e64fbc85d305641848e366807710
Tiago Murakami <trmurakami@gmail.com>
2021-06-10 11:23:48 -0300

    Alt

--- result_autores.php
+++ result_autores.php
@@ -113,7 +113,7 @@ $get_data = $_GET;
                                     <div class="p-2 bd-highlight"><a class="text-dark" href='result.php?filter[]=vinculo.lattes_id:"<?php echo $r['_source']['lattesID']; ?>"'>Ver produções indexadas</a></div>
                                     <div class="p-2 bd-highlight">
                                         <a class="text-dark" href="http://lattes.cnpq.br/<?php echo $r['_source']['lattesID']; ?>">
-                                            <img src="inc/images/logo-lattes.png" width="25px" height="25px">
+                                            <img src="inc/images/logo-lattes.png" width="25px" height="25px" alt="Acessar Currículo Lattes">
                                         </a>
                                     </div>
                                 </div>
