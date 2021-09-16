09214cddec68628d11249b9b24b9589ac0aa9a84
Tiago Murakami <trmurakami@gmail.com>
2021-09-16 16:03:32 -0300

    Redirect http to https

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    $location = 'https://unifesp.br/prodmais/index.php';
     header('HTTP/1.1 301 Moved Permanently');
     header('Location: ' . $location);
     exit;
