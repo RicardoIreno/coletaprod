c309069e9b25604229dd4e795a9f874d0fbe0ea9
Tiago Murakami <trmurakami@gmail.com>
2021-10-26 09:17:49 -0300

    Correção

--- tools/automatic_index.php
+++ tools/automatic_index.php
@@ -62,6 +62,35 @@ while (($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) != false) {
         $output = curl_exec($ch);
 
         unset($queryParams);
+        
+    } elseif ($row["COD_LATTES_16"] != null) {
+        $DataAtualizacaoLattes = file_get_contents('http://200.133.208.25/api/proxy_data_atualizacao/' . $row["COD_LATTES_16"] . '');
+        $DataAtualizacaoLattes_formatted = '' . substr($DataAtualizacaoLattes, 6, 4) . '-' . substr($DataAtualizacaoLattes, 3, 2) . '';
+
+        echo "Processando " . $row['COD_LATTES_16'];
+        echo "<br/><br/>";
+
+        $queryParams[] = '&tag=';
+        $queryParams[] = '&unidade=' . $row["DESC_ACADEMICA"] . '';
+        $queryParams[] = '&departamento=' . $row["DESC_DEPTO"] . '';
+        $queryParams[] = '&tipvin=' . $row["CARGO_REDUZIDO"] . '';
+        $queryParams[] = '&divisao=' . $row["DESC_DIV"] . '';
+        $queryParams[] = '&secao=' . $row["DESC_SEC"] . '';
+        $queryParams[] = '&genero=' . $row["GENERO"] . '';
+        $queryParams[] = '&desc_nivel=' . $row["DESCRICAO_NIVEL"] . '';
+        //$queryParams[] = '&desc_curso=' . $r['_source']['desc_curso'][0] . '';
+        $queryParams[] = '&campus=' . $row["DESC_GESTORA"] . '';
+        $queryParams[] = '&desc_gestora=' . $row["DESC_GESTORA"] . '';
+        $queryParams[] = '&dt_atual_lattes=' . $DataAtualizacaoLattes_formatted . '';
+        $queryParams[] = '&dt_credenciamento=' . date("Y", strtotime($row["DT_CREDENC"])) . '';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, '' . $url_base . '/import_lattes_to_elastic_dedup.php?lattesID=' . $row['COD_LATTES_16'] . '');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, implode('', $queryParams));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+
+        unset($queryParams);
     }
     unset($IDLattes);
 }
