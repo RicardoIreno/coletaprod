8ce543b7992cfdd0a26c82408b6d07e94dc7e60e
Tiago Murakami <trmurakami@gmail.com>
2021-11-10 20:28:21 -0300

    Correções

--- pre_dash.php
+++ pre_dash.php
@@ -66,7 +66,7 @@
                 </div>
 
                 <center>
-                    <img class="manual-img" src="assets/img/manual/login.jpg" />
+                    <img class="manual-img" src="inc/images/login.jpg" />
                 </center>
 
                 <p>O Prodmais utiliza a ferramenta Elastic Search para oferecer um poderoso dashboard com dados detalhados sobre a base de produção acadêmica. Você pode consultar duas categorias de dashboard: produção acadêmica e perfil dos pesquisadores.</p>
