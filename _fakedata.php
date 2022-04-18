914c5df43bb45f15b6f5923b1e28d51e0f4aa351
Ricardo Ireno <ricardoireno@gmail.com>
2022-04-18 13:11:14 -0300

    Exclusão de arquivos não usados e pasta Assets dentro do profile

---
+++ _fakedata.php
@@ -0,0 +1,429 @@
+<?php 
+  $tags = [
+    [
+      "name" =>  "População",
+      "weight" =>  3
+    ],
+    [
+      "name" =>  "Crise",
+      "weight" =>  4
+    ],
+    [
+      "name" =>  "Comércio",
+      "weight" =>  7
+    ],
+    [
+      "name" =>  "Legislação",
+      "weight" =>  2
+    ],
+    [
+      "name" =>  "Trabalho",
+      "weight" =>  3
+    ],
+    [
+      "name" =>  "Governança",
+      "weight" =>  4
+    ],
+    [
+      "name" =>  "Empregabilidade",
+      "weight" =>  5
+    ],
+    [
+      "name" =>  "Sociedade",
+      "weight" =>  1
+    ],
+    [
+      "name" =>  "Justiça",
+      "weight" =>  8
+    ],
+    [
+      "name" =>  "Direitos",
+      "weight" =>  9
+    ],
+    [
+      "name" =>  "Direitos humanos",
+      "weight" =>  4
+    ],
+    [
+      "name" =>  "Política",
+      "weight" =>  6
+    ],
+    [
+      "name" =>  "Informação",
+      "weight" =>  8
+    ],
+    [
+      "name" =>  "Redes sociais",
+      "weight" =>  5
+    ],
+    [
+      "name" =>  "Mundo digital",
+      "weight" =>  7
+    ],
+    [
+      "name" =>  "Saúde",
+      "weight" =>  4
+    ],
+    [
+      "name" =>  "Ecologia",
+      "weight" =>  8
+    ],
+    [
+      "name" =>  "Dinheiro",
+      "weight" =>  2
+    ],
+    [
+      "name" =>  "Polítias Públicas",
+      "weight" =>  1
+    ],
+    [
+      "name" =>  "Polícia",
+      "weight" =>  5
+    ],
+    [
+      "name" =>  "Empreendedorismo",
+      "weight" =>  6
+    ],
+    [
+      "name" =>  "Inclusão",
+      "weight" =>  8
+    ],
+    [
+      "name" =>  "Classes sociais",
+      "weight" =>  7
+    ],
+    [
+      "name" =>  "Antropologia",
+      "weight" =>  4
+    ],
+    [
+      "name" =>  "Recessão",
+      "weight" =>  6
+    ],
+    [
+      "name" =>  "Pirataria",
+      "weight" =>  1
+    ],
+    [
+      "name" =>  "Etnografia",
+      "weight" =>  3
+    ]
+  ];
+
+  $artigos_publicados = [
+    [
+      "year" =>  2021,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2020,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2019,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2018,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2017,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2016,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2015,
+      "total" =>  2
+    ]
+  ];
+
+  $livros_e_capitulos = [
+    [
+      "year" =>  2021,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2020,
+      "total" =>  1
+    ],
+    [
+      "year" =>  2019,
+      "total" =>  1
+    ],
+    [
+      "year" =>  2018,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2017,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2016,
+      "total" =>  0
+    ],
+    [
+      "year" =>  2015,
+      "total" =>  0
+    ]
+  ];
+
+$textos_jornais_ou_revistas = [
+    [
+      "year" =>  2021,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2020,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2019,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2018,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2017,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2016,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2015,
+      "total" =>  2
+    ]
+  ];
+
+  
+$orientacoes_mestrado = [
+    [
+      "year" =>  2021,
+      "total" =>  1
+    ],
+    [
+      "year" =>  2020,
+      "total" =>  1
+    ],
+    [
+      "year" =>  2019,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2018,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2017,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2016,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2015,
+      "total" =>  4
+    ]
+  ];
+
+
+$orientacoes_doutorado = [
+    [
+      "year" =>  2021,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2020,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2019,
+      "total" =>  1
+    ],
+    [
+      "year" =>  2018,
+      "total" =>  1
+    ],
+    [
+      "year" =>  2017,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2016,
+      "total" =>  5
+    ],
+    [
+      "year" =>  2015,
+      "total" =>  6
+    ]
+  ];
+
+
+$outras_orirentacoes = [
+    [
+      "year" =>  2021,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2020,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2019,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2018,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2017,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2016,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2015,
+      "total" =>  2
+    ]
+  ];
+
+
+$ensino = [
+    [
+      "year" =>  2021,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2020,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2019,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2018,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2017,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2016,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2015,
+      "total" =>  2
+    ]
+  ];
+
+$trabalhos_em_eventos = [
+    [
+      "year" =>  2021,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2020,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2019,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2018,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2017,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2016,
+      "total" =>  1
+    ],
+    [
+      "year" =>  2015,
+      "total" =>  1
+    ]
+  ];
+
+
+  $participacoes_eventos = [
+    [
+      "year" =>  2021,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2020,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2019,
+      "total" =>  4
+    ],
+    [
+      "year" =>  2018,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2017,
+      "total" =>  2
+    ],
+    [
+      "year" =>  2016,
+      "total" =>  3
+    ],
+    [
+      "year" =>  2015,
+      "total" =>  4
+    ]
+  ];
+
+
+  $softwares_patentes = [
+    [
+      "year" =>  2021,
+      "total" =>  1
+    ],
+    [
+      "year" =>  2020,
+      "total" =>  1
+    ],
+    [
+      "year" =>  2019,
+      "total" =>  0
+    ],
+    [
+      "year" =>  2018,
+      "total" =>  0
+    ],
+    [
+      "year" =>  2017,
+      "total" =>  0
+    ],
+    [
+      "year" =>  2016,
+      "total" =>  0
+    ],
+    [
+      "year" =>  2015,
+      "total" =>  0
+    ]
+  ];
+
+?>
