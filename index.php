148db11bcac0a7720b4dc161e19e9511a42292dd
Tiago Murakami <trmurakami@gmail.com>
2022-04-26 06:22:14 -0300

    Correção no funcionamento do botão

--- index.php
+++ index.php
@@ -118,80 +118,76 @@ com este programa, Se não, veja <http://www.gnu.org/licenses/>.
 
     <div id="mySearch" class="search">
 
-      <div class="u-div-v">
+      <form class="search-form" class="" action="result.php" title="Pesquisa simples">
 
-        <form class="search-form" class="" action="result.php" title="Pesquisa simples">
-
-          <input class="search-input" type="search" placeholder="Pesquise por palavra chave" aria-label="Pesquisar"
-            name="search">
-        </form>
-
-      </div>
-
-      <button v-on:click="changeSearch()" class="btn" title="Alternar modo de pesquisa">
-        <span v-if="searchPage == 'simple'">
-          <svg class="btn-ico" x="0px" y="0px" viewBox="0 0 80 48">
-            <path class="st0" d="M7.7,10c0.7,0,1.5,0.2,2.2,0.5L39.7,25l30.6-14c2.5-1.1,5.5,0,6.6,2.5c1.1,2.5,0,5.5-2.5,6.6l-32.7,15
-                c-1.4,0.6-2.9,0.6-4.3-0.1l-32-15.6C3,18.2,2,15.2,3.2,12.8C4,11,5.8,10,7.7,10z" />
-          </svg>
-        </span>
-        <span v-if="searchPage == 'advanced'">
-          <svg class="btn-ico" x="0px" y="0px" viewBox="0 0 80 48">
-            <path class="st0" d="M72.3,35.5c-0.7,0-1.5-0.2-2.2-0.5L40.3,20.5l-30.6,14c-2.5,1.1-5.5,0-6.6-2.5c-1.1-2.5,0-5.5,2.5-6.6l32.7-15
-                c1.4-0.6,2.9-0.6,4.3,0.1l32,15.6c2.5,1.2,3.5,4.2,2.3,6.7C76,34.5,74.2,35.5,72.3,35.5z" />
-          </svg>
-        </span>
-      </button>
-
-      <div class="u-div-v" v-if="searchPage == 'advanced'">
-
-        <form class="myform" action="result.php" title="Mais opções de pesquisa"></form>
-
-        <label class="u-info">Mais opções de pesquisa:</label>
-
-        <?php paginaInicial::filter_select("vinculo.ppg_nome"); ?>
-
-        <input class="search-input" type="search" placeholder="Filtrar por Nome do Programa de Pós-Graduação (Opcional)"
-          aria-label="Mudar" name="search">
-
-        <input class="search-input" list="datalistOptions" id="authorsDataList"
-          placeholder="Filtrar por nome ou ID Lattes do autor" name="filter[]" v-model="query" @input="searchCV()">
-
-        <datalist class="search-input" id="datalistOptions">
-          <option v-for="author in authors" :key="author._id" :value="'vinculo.lattes_id:' + author._id">
-            {{author._source.nome_completo}}
-          </option>
-        </datalist>
-
-        <label class="u-info">Filtrar por data:</label>
-        <div class="u-div-h">
-          <input type="text" class="search-date" id="initialYear" name="initialYear" pattern="\d{4}"
-            placeholder="Data inicial" />
-          <span> à </span>
-          <input type="text" class="search-date" id="finalYear" name="finalYear" pattern="\d{4}"
-            placeholder="Data final" />
-
+        <div class="u-div-v">
+          <input class="search-input" type="search" placeholder="Pesquise por palavra chave" aria-label="Pesquisar" name="search">       
         </div>
 
-        </form>
-      </div> <!-- advanced -->
+        <a v-on:click="changeSearch()" class="btn" title="Alternar modo de pesquisa">
+          <span v-if="searchPage == 'simple'">
+            <svg class="btn-ico" x="0px" y="0px" viewBox="0 0 80 48">
+              <path class="st0" d="M7.7,10c0.7,0,1.5,0.2,2.2,0.5L39.7,25l30.6-14c2.5-1.1,5.5,0,6.6,2.5c1.1,2.5,0,5.5-2.5,6.6l-32.7,15
+                  c-1.4,0.6-2.9,0.6-4.3-0.1l-32-15.6C3,18.2,2,15.2,3.2,12.8C4,11,5.8,10,7.7,10z" />
+            </svg>
+          </span>
+          <span v-if="searchPage == 'advanced'">
+            <svg class="btn-ico" x="0px" y="0px" viewBox="0 0 80 48">
+              <path class="st0" d="M72.3,35.5c-0.7,0-1.5-0.2-2.2-0.5L40.3,20.5l-30.6,14c-2.5,1.1-5.5,0-6.6-2.5c-1.1-2.5,0-5.5,2.5-6.6l32.7-15
+                  c1.4-0.6,2.9-0.6,4.3,0.1l32,15.6c2.5,1.2,3.5,4.2,2.3,6.7C76,34.5,74.2,35.5,72.3,35.5z" />
+            </svg>
+          </span>
+        </a>
 
-      <button type="submit" class="btnSearch" title="Buscar">
-        <svg class="btnSearch-ico" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100">
-          <path
-            d="M98.6,86.5L79.2,67c-0.9-0.9-2.1-1.4-3.3-1.4h-3.2c5.4-6.9,8.6-15.6,8.6-25C81.3,18.2,63.1,0,40.6,0
-              S0,18.2,0,40.6s18.2,40.6,40.6,40.6c9.4,0,18.1-3.2,25-8.6v3.2c0,1.3,0.5,2.4,1.4,3.3l19.5,19.5c1.8,1.8,4.8,1.8,6.6,0l5.5-5.5
-              C100.5,91.3,100.5,88.3,98.6,86.5z M40.6,65.6c-13.8,0-25-11.2-25-25s11.2-25,25-25s25,11.2,25,25S54.5,65.6,40.6,65.6z" />
-        </svg>
-      </button>
+        <div class="u-div-v" v-if="searchPage == 'advanced'">
 
-      <small class="u-info"><b>Dicas</b></small>
-      <small class="u-info">1: Use * para busca por radical. Ex: biblio*.</small>
-      <small class="u-info">2: Para buscas exatas, coloque entre "". Ex: "Direito civil"</small>
-      <small class="u-info">3: Por padrão, o sistema utiliza o operador booleano OR. Caso necessite deixar a busca
-        mais específica, utilize o operador AND (em maiúscula)</small>
+          <label class="u-info">Mais opções de pesquisa:</label>
 
-    </div> <!-- app -->
+          <?php paginaInicial::filter_select("vinculo.ppg_nome"); ?>
+
+          <input class="search-input" type="search" placeholder="Filtrar por Nome do Programa de Pós-Graduação (Opcional)"
+            aria-label="Mudar" name="search">
+
+          <input class="search-input" list="datalistOptions" id="authorsDataList"
+            placeholder="Filtrar por nome ou ID Lattes do autor" name="filter[]" v-model="query" @input="searchCV()">
+
+          <datalist class="search-input" id="datalistOptions">
+            <option v-for="author in authors" :key="author._id" :value="'vinculo.lattes_id:' + author._id">
+              {{author._source.nome_completo}}
+            </option>
+          </datalist>
+
+          <label class="u-info">Filtrar por data:</label>
+          <div class="u-div-h">
+            <input type="text" class="search-date" id="initialYear" name="initialYear" pattern="\d{4}"
+              placeholder="Data inicial" />
+            <span> à </span>
+            <input type="text" class="search-date" id="finalYear" name="finalYear" pattern="\d{4}"
+              placeholder="Data final" />
+
+          </div>
+
+          
+        </div> <!-- advanced -->
+
+        <button type="submit" class="btnSearch" title="Buscar">
+          <svg class="btnSearch-ico" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100">
+            <path
+              d="M98.6,86.5L79.2,67c-0.9-0.9-2.1-1.4-3.3-1.4h-3.2c5.4-6.9,8.6-15.6,8.6-25C81.3,18.2,63.1,0,40.6,0
+                S0,18.2,0,40.6s18.2,40.6,40.6,40.6c9.4,0,18.1-3.2,25-8.6v3.2c0,1.3,0.5,2.4,1.4,3.3l19.5,19.5c1.8,1.8,4.8,1.8,6.6,0l5.5-5.5
+                C100.5,91.3,100.5,88.3,98.6,86.5z M40.6,65.6c-13.8,0-25-11.2-25-25s11.2-25,25-25s25,11.2,25,25S54.5,65.6,40.6,65.6z" />
+          </svg>
+        </button>
+
+        <small class="u-info"><b>Dicas</b></small>
+        <small class="u-info">1: Use * para busca por radical. Ex: biblio*.</small>
+        <small class="u-info">2: Para buscas exatas, coloque entre "". Ex: "Direito civil"</small>
+        <small class="u-info">3: Por padrão, o sistema utiliza o operador booleano OR. Caso necessite deixar a busca
+          mais específica, utilize o operador AND (em maiúscula)</small>
+
+      </div>
+    </form>
+     <!-- app -->
 
     <hr />
     <hr />
