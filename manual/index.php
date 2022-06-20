3aae464970504d3e1cd39cfcfd7fe9d10e5442e1
Tiago Murakami <trmurakami@gmail.com>
2022-06-20 04:53:00 -0300

    Correção no manual

--- manual/index.php
+++ manual/index.php
@@ -48,7 +48,7 @@
         <p>O Prodmais é uma ferramenta que agrega informação de diversas fontes, mas não realiza processamento posterior nelas. Neste manual prático você podrá tirar suas dúvidas sobre como utilizar o Prodmais. É fácil efetuar as suas buscas no Prodmais, e você pode fazer utilizando um dos seguintes caminhos:</p>
       </div>
 
-      <?php require './manual-menu.php'; ?>
+      <?php require 'manual-menu.php'; ?>
       <div class="u-mb-2"></div>
 
     </main>
