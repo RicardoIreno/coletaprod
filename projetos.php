05355f6175f0be2e35f9729e5f929864c82eb44c
Ricardo Ireno <ricardoireno@outlook.com>
2022-08-11 16:56:09 -0300

    Protótipo da página que lista projetos

---
+++ projetos.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+
+<?php
+$arrProjetos = '{
+	"projetos": [
+    {
+      "nome": "Caracterização química, funcional e potencial biotecnológico de mix balanceado de resíduo vegetal associado a subprodutos de origem vegetal",
+      "descricao": "O setor alimentício produz grande quantidade de lixo, tanto liquido como sólidos provenientes da produção, preparação e consumo de alimentos. Por sua composição, os resíduos provenientes de vegetais e frutas podem ser uma matéria prima de alto valor agregado para produção de novos produtos. Há grande interesse na utilização de fontes naturais no processamento de alimentos. Estudos apresentam substituição dos ingredientes sintéticos por aditivos naturais particularmente derivados de plantas e subprodutos agroindustriais. Bioconservação (usando compostos fenólicos) também está sendo objeto de diversos estudos como uma alternativa eficiente para aditivos alimentares. A avaliação da bioacessibilidade de compostos bioativos de vegetais vem sendo de grande interesse. Associar o potencial funcional e biotecnológico da FFH e demais resíduos vegetais com as propriedades funcionais de subprodutos da jabuticabeira é o objetivo deste trabalho. Serão identificados resíduos mais produzidos em UAN´s por processo mínimo. Estes associados a resíduo de mix de vegetal e frutas gerado na produção de bebida isotônica e subproduto de jabuticabeira serão base na elaboração de mix balanceado(MB). Será feita a caracterização química e funcional do MB e o mesmo será utilizado como substrato para produção de biomassa.. ",
+      "coordenacao": "Edira Castello Branco de Andrade Gonçalves",
+      "concluido": "false",
+      "natureza": "pesquisa",
+      "alunos_graducacao": "1",
+      "alunos_mestrado_academico": "1",
+      "alunos_doutorado": "1",
+      "financiamento": "",
+      "financiadores": "Fundação Carlos Chagas Filho de Amparo à Pesquisa do Estado do RJ",
+      "Número-producoes": "31",
+      "ano_inicial": "2018",
+      "ano_final": "2022",
+      "integrantes": [
+        "Roberta Melquiades Silva de Andrade",
+        "Pedro Paulo Saldanha Coimbra",
+        "Nathânia de Sá Mendes",
+        "Tamara Righetti Tupini Cavalheiro",
+        "Elisa Dávila Costa Cavalcanti"
+      ]
+    },
+    {
+      "nome": "Projeto Exemplo 2 - Um limiar do inconsciente retórico composto da interpretação transcultural da realidade percebida",
+      "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "coordenacao": "Acácio Florenço De Oliveira Camprestre",
+      "concluido": "true",
+      "natureza": "pesquisa",
+      "alunos_graducacao": "1",
+      "alunos_mestrado_academico": "1",
+      "alunos_doutorado": "1",
+      "financiamento": "",
+      "financiadores": "Umbrella Academy",
+      "Número-producoes": "31",
+      "ano_inicial": "2000",
+      "ano_final": "",
+      "integrantes": [
+        "José Pires Fonseca",
+        "Roberta Melquiades Silva de Andrade",
+        "Pedro Paulo Saldanha Coimbra",
+        "Lúcia Maria",
+        "Nathânia de Sá Mendes",
+        "Tamara Righetti Tupini Cavalheiro",
+        "Elisa Dávila Costa Cavalcanti",
+        "John Doe",
+        "Maria Lúcia",
+        "Acácio Florenço De Oliveira Camprestre"
+      ]
+    },
+    {
+      "nome": "Projeto Exemplo",
+      "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "coordenacao": "Jon Doe",
+      "concluido": "false",
+      "natureza": "pesquisa",
+      "alunos_graducacao": "1",
+      "alunos_mestrado_academico": "1",
+      "alunos_doutorado": "1",
+      "financiamento": "",
+      "financiadores": "Umbrella Academy",
+      "Número-producoes": "31",
+      "ano_inicial": "2010",
+      "ano_final": "",
+      "integrantes": [
+        "José Henrique Marino",
+        "Antônio de Andrade Costa",
+        "Pedro Alcantara de Neves",
+        "Lúcia Maria",
+        "John Doe",
+        "Maria Lúcia",
+        "Acácio Florenço De Oliveira Camprestre"
+      ]
+    },
+    {
+      "nome": "Caracterização química, funcional e potencial biotecnológico de mix balanceado de resíduo vegetal associado a subprodutos de origem vegetal",
+      "descricao": "O setor alimentício produz grande quantidade de lixo, tanto liquido como sólidos provenientes da produção, preparação e consumo de alimentos. Por sua composição, os resíduos provenientes de vegetais e frutas podem ser uma matéria prima de alto valor agregado para produção de novos produtos. Há grande interesse na utilização de fontes naturais no processamento de alimentos. Estudos apresentam substituição dos ingredientes sintéticos por aditivos naturais particularmente derivados de plantas e subprodutos agroindustriais. Bioconservação (usando compostos fenólicos) também está sendo objeto de diversos estudos como uma alternativa eficiente para aditivos alimentares. A avaliação da bioacessibilidade de compostos bioativos de vegetais vem sendo de grande interesse. Associar o potencial funcional e biotecnológico da FFH e demais resíduos vegetais com as propriedades funcionais de subprodutos da jabuticabeira é o objetivo deste trabalho. Serão identificados resíduos mais produzidos em UAN´s por processo mínimo. Estes associados a resíduo de mix de vegetal e frutas gerado na produção de bebida isotônica e subproduto de jabuticabeira serão base na elaboração de mix balanceado(MB). Será feita a caracterização química e funcional do MB e o mesmo será utilizado como substrato para produção de biomassa.. ",
+      "coordenacao": "Edira Castello Branco de Andrade Gonçalves",
+      "concluido": "false",
+      "natureza": "pesquisa",
+      "alunos_graducacao": "1",
+      "alunos_mestrado_academico": "1",
+      "alunos_doutorado": "1",
+      "financiamento": "",
+      "financiadores": "Fundação Carlos Chagas Filho de Amparo à Pesquisa do Estado do RJ",
+      "Número-producoes": "31",
+      "ano_inicial": "2018",
+      "ano_final": "2022",
+      "integrantes": [
+        "Roberta Melquiades Silva de Andrade",
+        "Pedro Paulo Saldanha Coimbra",
+        "Nathânia de Sá Mendes",
+        "Tamara Righetti Tupini Cavalheiro",
+        "Elisa Dávila Costa Cavalcanti"
+      ]
+    },
+    {
+      "nome": "Projeto Exemplo 2 - Um limiar do inconsciente retórico composto da interpretação transcultural da realidade percebida",
+      "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "coordenacao": "Acácio Florenço De Oliveira Camprestre",
+      "concluido": "true",
+      "natureza": "pesquisa",
+      "alunos_graducacao": "1",
+      "alunos_mestrado_academico": "1",
+      "alunos_doutorado": "1",
+      "financiamento": "",
+      "financiadores": "Umbrella Academy",
+      "Número-producoes": "31",
+      "ano_inicial": "2000",
+      "ano_final": "",
+      "integrantes": [
+        "José Pires Fonseca",
+        "Roberta Melquiades Silva de Andrade",
+        "Pedro Paulo Saldanha Coimbra",
+        "Lúcia Maria",
+        "Nathânia de Sá Mendes",
+        "Tamara Righetti Tupini Cavalheiro",
+        "Elisa Dávila Costa Cavalcanti",
+        "John Doe",
+        "Maria Lúcia",
+        "Acácio Florenço De Oliveira Camprestre"
+      ]
+    },
+    {
+      "nome": "Projeto Exemplo",
+      "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "coordenacao": "Jon Doe",
+      "concluido": "false",
+      "natureza": "pesquisa",
+      "alunos_graducacao": "1",
+      "alunos_mestrado_academico": "1",
+      "alunos_doutorado": "1",
+      "financiamento": "",
+      "financiadores": "Umbrella Academy",
+      "Número-producoes": "31",
+      "ano_inicial": "2010",
+      "ano_final": "",
+      "integrantes": [
+        "José Henrique Marino",
+        "Antônio de Andrade Costa",
+        "Pedro Alcantara de Neves",
+        "Lúcia Maria",
+        "John Doe",
+        "Maria Lúcia",
+        "Acácio Florenço De Oliveira Camprestre"
+      ]
+    }  
+  ]
+}';
+
+class ListProjetos {
+  static function listAll($data) {
+    $arr = json_decode($data);
+    $projetos = $arr -> projetos ;
+
+    // var_dump($projetos );
+   
+    foreach( $projetos as $p ) {
+
+      $p -> concluido === "true" ? $concluido = "Projeto Concluído" : $concluido = "";
+
+      SList::genericItem(
+        $type = 'ppg',
+        $itemName = $p -> nome,
+        $itemNameLink = '',
+        $itemInfoA = substr($p -> descricao, 0, 100) . '...',
+        $itemInfoB = '<i>Financiado por:</i> ' . $p -> financiadores,
+        $itemInfoC = '<i>Coordenação:</i> ' . $p -> coordenacao,
+        $itemInfoD = '<i>Integrantes:</i> ' . implode(', ', $p -> integrantes),
+        $itemInfoE = $concluido,
+        $yearStart = $p -> ano_inicial,
+        $yearEnd = $p -> ano_final
+      );
+    }
+  }
+}
+        
+?>
+
+<head>
+  <?php
+        require 'inc/config.php';
+        require 'inc/meta-header.php';
+        require 'inc/functions.php';
+        require 'components/SList.php';
+        require 'components/TagCloud.php';
+        require '_fakedata.php';
+        ?>
+  <meta charset="utf-8" />
+  <title><?php echo $branch; ?> - Projetos</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+  <meta name="description" content="Prodmais Unifesp." />
+  <meta name="keywords" content="Produção acadêmica, lattes, ORCID" />
+  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+  <link rel="stylesheet" href="sass/main.css" />
+</head>
+
+<body class="c-wrapper-body">
+  <?php if(file_exists('inc/google_analytics.php')){include 'inc/google_analytics.php';}?>
+
+  <?php require 'inc/navbar.php'; ?>
+
+  <main class="c-wrapper-container">
+    <div class="c-wrapper-paper">
+      <div class="c-wrapper-inner">
+        <h1 class=" t t-h1 u-mb-2">Programas de Pós-Graduação</h1>
+
+        <section class="c-inputs u-lg-up-row">
+          <div class="u-md-up-row">
+            <div class="c-switch-container">
+              <input id="switch-projetos-concluidos" class="c-switch c-switch-shadow" type="checkbox" />
+              <label for="switch-projetos-concluidos"></label>
+              <span class="c-switch-text"> Concluídos </span>
+            </div>
+            <div class="c-switch-container">
+              <input id="switch-projetos-andamento" class="c-switch c-switch-shadow" type="checkbox" />
+              <label for="switch-projetos-andamento"></label>
+              <span class="c-switch-text"> Em andamento </span>
+            </div>
+          </div>
+
+          <input class="c-input" type="text" />
+          <button class="c-btn">Pesquisar</button>
+
+        </section>
+
+        <section class="u-my-4">
+          <h2 class="t t-h2 u-mb-2">Palavras mais frequentes</h3>
+            <?php Tag::cloud($categorysFake) ?>
+        </section>
+
+        <hr class="c-line u-mb-4" />
+
+        <section class=" p-projetos-container">
+
+          <div class="p-projetos-lista">
+            <?php ListProjetos::listAll($arrProjetos); ?>
+          </div>
+        </section>
+
+      </div>
+    </div>
+  </main>
+
+  <?php include('inc/footer.php'); ?>
+  <script>
+  let filterConcluido = document.getElementById('switch-projetos-concluidos')
+  let filterAndamento = document.getElementById('switch-projetos-andamento')
+
+  filterConcluido.addEventListener("click", function() {
+    if (filterAndamento.checked === true) filterAndamento.checked = false
+  })
+
+  filterAndamento.addEventListener("click", function() {
+    if (filterConcluido.checked === true) filterConcluido.checked = false
+  })
+  </script>
+</body>
+
+</html>
