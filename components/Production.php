25343758309551a2c13329e5dd75bc039a8a6f01
Ricardo Ireno <ricardoireno@outlook.com>
2022-08-30 14:35:27 -0300

    Ajustes no espaçamento do componente Production

--- components/Production.php
+++ components/Production.php
@@ -43,7 +43,7 @@ class Production
   {
     return "
         <a class='t t-a d-icon-text' href='https://doi.org/$url' target='blank'>
-        <img class='i-doi' src='inc/images/logos/doi.svg' title='doi' alt='doi' />
+        <img class='i-doi u-ml-05' src='inc/images/logos/doi.svg' title='doi' alt='doi' />
         </a>";
   }
 
@@ -51,7 +51,7 @@ class Production
   {
     return "
         <a class='t t-a d-icon-text' href='$url' target='blank'> 
-          <i class='i i-link i-link' title='Conteúdo completo' alt='Conteúdo completo'></i>
+          <i class='i i-link i-link u-ml-05' title='Conteúdo completo' alt='Conteúdo completo'></i>
           Conteúdo completo
         </a>";
   }
@@ -60,7 +60,7 @@ class Production
   {
     return "
         <a class='t t-a d-icon-text' href='$url' target='blank'> 
-          <i class='i i-link i-link' title='ISSN' alt='ISSN'></i>
+          <i class='i i-link i-link u-ml-05' title='ISSN' alt='ISSN'></i>
           ISSN: $url
         </a>";
   }
@@ -109,6 +109,7 @@ class Production
             $urlRendered	
             $issnRendered					
 					</div>
+          $datePublished
 					
 					<p class='t t-light'>
 						Fonte: $refName $refVol $refFascicle $refPage
