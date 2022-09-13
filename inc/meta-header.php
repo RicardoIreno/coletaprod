4fcfe134beb5184f75e389b9b5618a004f738c3c
Tiago Murakami <trmurakami@gmail.com>
2022-09-13 09:57:02 -0300

    Correção vue.min.js

--- inc/meta-header.php
+++ inc/meta-header.php
@@ -7,7 +7,7 @@
 <script src="inc/js/vue.js"></script><!-- https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js -->
 <!--Production -->
 <!--
-<script src="inc/js/vue.min.js">
+<script src="inc/js/vue.min.js"></script>
 -->
 
 <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
