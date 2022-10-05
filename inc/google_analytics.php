db505a5e6771dfc09f215d26d5015b1a9883844a
Tiago Murakami <trmurakami@gmail.com>
2022-10-05 07:17:36 -0300

    Exclusão do google analytics

--- inc/google_analytics.php
+++ inc/google_analytics.php
@@ -1,9 +1,1 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-8RFYFPL967"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-8RFYFPL967');
-</script>
+<?php // Inclua aqui o código do google analytics ?>
